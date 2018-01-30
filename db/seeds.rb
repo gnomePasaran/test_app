aefe724062cd6bba764e44bdd78acbb01b9d3e7c
Alexander Ivahnenko <alexander_i@fast-report.com>
2018-01-31 01:14:55 +0300

    add seed for Goods with parsing from xlsx file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'creek'
+
+goods = []
+
+file_path = Rails.root.join('spec', 'support', 'goods.xlsx')
+
+unless File.exist?(file_path)
+  puts 'File dose not exist!'
+  return
+end
+
+creek = Creek::Book.new(file_path)
+sheet = creek.sheets[0]
+
+dates = sheet.rows.first.except('A1').values
+
+ActiveRecord::Base::transaction do
+  sheet.rows.to_a.last(sheet.rows.count - 1).each do |row|
+    next unless row.any?
+
+    title = row.first[1]
+    row.except(row.first[0]).each_with_index do |cell, i|
+      goods << { title: title, date: dates[i], revenue: cell[1].to_f }
+    end
+  end
+
+  Goods.create(goods)
+  puts 'Well done!'
+end
